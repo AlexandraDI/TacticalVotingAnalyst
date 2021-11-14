01c34250ddb8cd83cb33ab23e213320371dae089
NielsOsnabrugge <nielsosnabrugge@gmail.com>
2021-11-14 15:00:37 +0100

    Added voting situation generator

---
+++ VotingSituation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class VotingSituation:
+    def __init__(self, voter_am, candidate_am):
+        voting_vector = np.arange(1, candidate_am + 1)
+        self.voting_matrix = np.repeat(voting_vector, repeats=voter_am).reshape((candidate_am, voter_am))
+        self.shufflevote()
+
+    def shufflevote(self):
+        np.array([np.random.shuffle(x) for x in self.voting_matrix.T])
