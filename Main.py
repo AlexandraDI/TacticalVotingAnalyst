32d00249cba3407b9099168d6fe4311e05712a85
lukassanting <43905056+lukassanting@users.noreply.github.com>
2021-11-26 12:47:55 +0100

    added Main

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 from VotingSituation import VotingSituation
+from Vot_Scheme import compute_vot_scheme
 
+def getOutcome(voters, candidates):
+    preferencesMatrix = VotingSituation(voters, candidates).voting_matrix
+    print(preferencesMatrix[0])
+    
 
-test = VotingSituation(6, 4)
-print(test.voting_matrix)
+# make function to take preferences and voting-scheme, outputs non strategic outcome
+# calculate happiness based on non-strategic outcome
