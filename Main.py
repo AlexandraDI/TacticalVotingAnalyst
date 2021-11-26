807547e60891465d183444d67da127095f0fd238
GianlucaVico <ganlucavico@gmail.com>
2021-11-26 13:13:15 +0100

    .

--- Main.py
+++ Main.py
@@ -1,10 +1,23 @@
 from VotingSituation import VotingSituation
-from Vot_Scheme import VotinngScheme
+from Vot_Scheme import VotingScheme, compute_vot_scheme
+import numpy as np
 
-def getOutcome(voters, candidates):
+def getOutcome(voters, candidates, scheme_type):
+
+    scheme = compute_vot_scheme(scheme_type, candidates)
+
     preferencesMatrix = VotingSituation(voters, candidates).voting_matrix
-    print(preferencesMatrix[0])
 
+    votes_per_candidate = [0 for i in range(candidates)]
+
+    for voter in range(voters):
+        for pref in range(candidates):
+            i = preferencesMatrix[pref, voter]
+            print(i)
+            votes_per_candidate[i - 1] += scheme[pref]
+
+    print(preferencesMatrix[0])
+    return votes_per_candidate
 
 # make function to take preferences and voting-scheme, outputs non strategic outcome
 # calculate happiness based on non-strategic outcome
